7e915b532cfba418b472352f15ad230b5fb6c75e
play.js <git@gituser.com>
2020-05-19 17:36:22 -0500

    importing js/separating icons

---
+++ icons.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const e = React.createElement;
+
+class iconClass extends React.Component{
+    constructor(props){
+        super(props);    
+    }
+    
+    render(){
+        return(
+            <h1>Icon class placeholder</h1>
+        );
+    }
+}
+
+var iconVariablePlaceholder="<h1>Icon class placeholder</h1>"
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import * as iconModule from "icons.js";
+
 const e = React.createElement;
 var colors=["blue", "green", "orange"];
 
